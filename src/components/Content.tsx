4e773e43044c23094f9bf8a3b3e1a97541bdc234
Israel Sampaio <israelsampaio28@gmail.com>
2022-02-11 20:34:27 -0400

    feat: slices App.tsx to Content and SideBar component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieProps } from "@/interfaces/Movie.interface";
+import { GenreResponseProps } from "@/interfaces/Genre.interface";
+
+import { MovieCard } from "./MovieCard";
+
+import "@/styles/content.scss";
+
+type ContentProps = {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+};
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
